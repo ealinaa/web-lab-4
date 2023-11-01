47e4b883785dedccf968c031b7a3fdde0ba5e5f8
Alina Adhikari <anishadhikari61@gmail.com>
2023-11-01 12:39:31 +0545

    web lab 4 added

---
+++ javascripts.js
@@ -0,0 +1,22 @@
+const blogs = [
+    {
+        id: 1,
+        title: "Essential Of JavaScript",
+        img: "https://assets.iorbex.com/blog/wp-content/uploads/2019/08/04143047/demo-account.png",
+        authorName: "Alina Adhikari",
+        createdDate: "2023-09-01T20:23:05Z",
+        readBy: 200,
+        commentsNo: 50,
+        status: "Published"
+    },
+    {
+        id: 2,
+        title: "How To Get Start With React",
+        img: "https://assets.iorbex.com/blog/wp-content/uploads/2019/08/04143047/demo-account.png",
+        authorName: "Ram shrestha",
+        createdDate: "2023-10-10T12:15:10Z",
+        readBy: 180,
+        commentsNo: 25,
+        status: "Archived"
+    },
+];
